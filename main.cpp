b659cbf7d5ae60ab6273208ba0d1e767708f095c
JeffBobbo <james.delph@hotmail.co.uk>
2015-06-23 13:58:04 +0100

    rewritten presentOptions to use colours, added the ability to heal yourself (using str and def attributes)

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "soul.h"
 #include "player.h"
 #include "console.h"
+#include "misc.h"
 
 bool cleanName(std::string str)
 {
@@ -12,31 +13,38 @@ bool cleanName(std::string str)
   return str.find_first_not_of(filter) == std::string::npos;
 }
 
-std::string presentOptions(Player* p, Soul* s)
+enum OPTIONS
 {
-  std::stringstream ss;
-  ss << p->getName() << "'s status:\n";
-  ss << "Level: " << p->getLevel() << "\n";
-  ss << "Atk: " << p->getAtk() << ", Str: " << p->getStr() << ", Def: " << p->getDef() << "\n";
-  ss << "HP: " << p->getHP() << "/" << p->getHPMax() << " (" << static_cast<int16_t>(p->getHPPerc() * 100.0) << "%)\n\n";
+  PROG = 1,
+  HEAL
+};
+
+void presentOptions(Player* p, Soul* s)
+{
+  output(p->getName() + "'s status:", 0, 0);
+  output(std::string("Level: ") + toString(p->getLevel()), 0, 1);
+  output(std::string("Atk: ") + toString(p->getAtk()) + ", Str: " + toString(p->getStr()) + ", Def: " + toString(p->getDef()), 0, 2);
+  output(std::string("HP: ") + toString(p->getHP()) + "/" + toString(p->getHPMax()) + " (" + toString(static_cast<int16_t>(p->getHPPerc() * 100.0)) + "%)", 0, 3, (p->getHPPerc() >= 0.7 ? FG_GREEN : p->getHPPerc() >= 0.3 ? FG_YELLOW : FG_RED));
 
   if (s)
   {
-    ss << "Gremlin's status:\n";
-    ss << "Level: " << s->getLevel() << "\n";
-    ss << "Atk: " << s->getAtk() << ", Str: " << s->getStr() << ", Def: " << s->getDef() << "\n";
-    ss << "HP: " << s->getHP() << "/" << s->getHPMax() << " (" << static_cast<int16_t>(s->getHPPerc() * 100.0) << "%)\n\n";
+    uint16_t x = getConsoleSize().x >> 1;
+    output("Gremlin's status:", x, 0);
+    output(std::string("Level: ") + toString(s->getLevel()), x, 1);
+    output(std::string("Atk: ") + toString(s->getAtk()) + ", Str: " + toString(s->getStr()) + ", Def: " + toString(s->getDef()), x, 2);
+    output(std::string("HP: ") + toString(s->getHP()) + "/" + toString(s->getHPMax()) + " (" + toString(static_cast<int16_t>(s->getHPPerc() * 100.0)) + "%)", x, 3, (s->getHPPerc() >= 0.7 ? FG_GREEN : s->getHPPerc() >= 0.3 ? FG_YELLOW : FG_RED));
   }
 
-  ss << "Available actions:\n";
+  output("Available actions:", 0, 5);
+  uint16_t opt = 6;
   if (s)
-    ss << "[1] Attack\n";
+    output("[" + toString(PROG) + "] Attack", 0, opt++);
   else
-    ss << "[1] Walk forwards\n";
-  ss << "[0] quit\n";
-  ss << "[quit] quit\n";
-
-  return ss.str();
+    output("[" + toString(PROG) + "] Walk forwards", 0, opt++);
+  if (p->getHPPerc() < 1.0)
+  output("[" + toString(HEAL) + "] Heal", 0, opt++);
+  output("[quit] quit", 0, opt++);
+  output("> ", 0, opt++);
 }
 
 int main(int argc, char** argv)
@@ -46,6 +54,7 @@ int main(int argc, char** argv)
     argv[i] = argv[i];
   }
 
+  clearConsole();
   std::cout << "Welcome to ConsoleCrawler!" << std::endl << std::endl << "Name yourself, explorer!" << std::endl << "> ";
 
   Player* player = nullptr;
@@ -57,48 +66,54 @@ int main(int argc, char** argv)
       std::getline(std::cin, name);
 
       if (cleanName(name))
-      {
-        std::cout << "I hope you're ready, " << name << std::endl;
         player = new Player(100, 100, 1, 1, 1, name); // yes I'm a horrible person who doesn't use c++11 memory management
-      }
       else
-      {
         std::cout << "Sorry, I didn't get that, try something else?" << std::endl << "> ";
-      }
     }
   }
 
+  clearConsole();
   Soul* gremlin = nullptr;
   while (true)
   {
-    std::cout << presentOptions(player, gremlin);
+    presentOptions(player, gremlin);
     std::cout << std::flush;
     std::string input;
     std::getline(std::cin, input);
-    clearConsole();
 
     if (input == "quit")
       break;
-    if (input == "1")
+
+    switch (toInt(input))
     {
-      if (!gremlin)
+      case PROG:
       {
-        gremlin = new Soul(10, 10, 1, 1, 1); // a weak gremlin
-        std::cout << "A wild gremlin appears!" << std::endl;
-      }
-      else
-      {
-        gremlin->makeImpact(player);
-        if (player->getHP() == 0.0)
-          break;
-        player->makeImpact(gremlin);
-        if (gremlin->getHP() == 0.0)
+        if (!gremlin)
         {
-          delete gremlin;
-          gremlin = nullptr;
+          gremlin = new Soul(10, 10, 1, 1, 1); // a weak gremlin
+          std::cout << "A wild gremlin appears!" << std::endl;
+        }
+        else
+        {
+          gremlin->makeImpact(player);
+          if (player->getHP() == 0.0)
+            break;
+          player->makeImpact(gremlin);
+          if (gremlin->getHP() == 0.0)
+          {
+            delete gremlin;
+            gremlin = nullptr;
+          }
         }
       }
+      break;
+      case HEAL:
+      {
+        player->makeImpact(player, true);
+      }
+      break;
     }
+    clearConsole();
   }
 
   if (gremlin)
